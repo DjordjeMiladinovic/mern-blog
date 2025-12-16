cb679f4cdf3feec15fc01cede2c35d7b8a7cf4e0
DjordjeMiladinovic <miladinovicdjordje03@gmail.com>
2025-12-16 15:15:41 +0100

    create post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,43 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import React from 'react'
-import SignIn from './pages/SignIn.jsx'
-import SignUp from './pages/SignUp.jsx'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import Dashboard from './pages/Dashboard.jsx'
-import Projects from './pages/Projects.jsx'
-import Header from './components/Header.jsx'
-import { Footer } from 'flowbite-react'
-import PrivateRoute from './components/PrivateRoute.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import PrivateRoute from './components/PrivateRoute';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
+import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
+import PostPage from './pages/PostPage';
+import ScrollToTop from './components/ScrollToTop';
+import Search from './pages/Search';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <ScrollToTop />
+      <Header />
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route element={<PrivateRoute />}>
-      <Route path="/dashboard" element={<Dashboard />} />
-      </Route>
-      <Route path="/projects" element={<Projects />} />
-    </Routes>
-    
-    <Footer/>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
+        </Route>
 
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/post/:postSlug' element={<PostPage />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
